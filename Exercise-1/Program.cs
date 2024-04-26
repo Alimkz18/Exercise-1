ff34617843b33a6030cfa931bf028363969fe3e8
zhain <zhain@NIGGER>
2024-04-25 21:00:00 -0500

    Add project files.

---
+++ Exercise-1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Alim Almatovich Zhainakov";
+            string email = "zhainakov.alim@mail.ru";
+
+            int age = 18;
+            double itScore = 78.5;
+            double mathScore = 65.8;
+            double physicsScore = 98.6;
+            int totalClasses = 3;
+
+
+
+            string pattern = "fullName: {0} \nage: {1} \nemail: {2} \nitScore: {3} \nmathScore {4} \nphysicsScore {5}";
+            WriteLine(pattern,
+                 fullName,
+                 age,
+                 email,
+                 itScore,
+                 mathScore,
+                 physicsScore);
+
+            double sum = itScore + mathScore + physicsScore;
+
+            double average = sum / totalClasses;
+
+            WriteLine($"Avarage: {average}");
+
+            ReadLine();
+
+        }
+    }
+}
